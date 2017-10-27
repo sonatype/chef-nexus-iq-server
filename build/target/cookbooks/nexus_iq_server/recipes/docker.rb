32f0fc69dd99264c7e27989c6de83ead06ec6d5d
bigspotteddog <bink.lynch@gmail.com>
2017-10-27 09:55:39 -0700

    Remove build folder that should not have been checked in.

--- build/target/cookbooks/nexus_iq_server/recipes/docker.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook:: nexus_iq_server
-# Recipe:: default
-#
-# Copyright:: Copyright (c) 2017-present Sonatype, Inc. Apache License, Version 2.0.
-
-include_recipe 'java'
-include_recipe 'nexus_iq_server::download'
-include_recipe 'nexus_iq_server::configure'
