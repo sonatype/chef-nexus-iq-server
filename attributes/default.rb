f85aba30ef6108035de3d3dd662216886f344a63
Joseph Cava <bink.lynch@gmail.com>
2018-02-02 12:57:17 -0800

    Update IQ Server to 1.43.0-01 - INT-444 (#8)

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@ default['java']['jdk_version'] = '8'
 default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['nexus_iq_server']['version'] = '1.42.0-01'
-default['nexus_iq_server']['checksum'] = '4ee713e5ddfea787ee366aae7adf1eb79f0ccea43d7bef96ae06fb535b13eb9e'
+default['nexus_iq_server']['version'] = '1.43.0-01'
+default['nexus_iq_server']['checksum'] = '91f0ad04ca0b3d99bc4384da11147abac75c6c0a9fa1201c2c44dd09731a62f0'
 default['nexus_iq_server']['install_dir'] = '/opt/sonatype/nexus-iq-server'
 default['nexus_iq_server']['logs_dir'] = '/var/log/nexus-iq-server'
 default['nexus_iq_server']['conf_dir'] = '/etc/nexus-iq-server'
