2d288261b0f1e4c866de098b3c4852ae28d03aa8
Joseph Cava <bink.lynch@gmail.com>
2018-02-06 16:26:36 -0800

    Updated [nexus-iq-server][config] structure for NXIQ-1.43+ - INT-445 (#9)
    * Updated the [nexus-iq-server][config] to match the new config.yml structure.
    
    * Change the archiveFileCount value in the docker recipe to 5.

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -4,6 +4,9 @@
 #
 # Copyright:: Copyright (c) 2017-present Sonatype, Inc. Apache License, Version 2.0.
 
+node.default['nexus_iq_server']['config']['server']['requestLog']['appenders'][0]['archivedFileCount'] = 5
+node.default['nexus_iq_server']['config']['logging']['appenders'][1]['archivedFileCount'] = 5
+
 include_recipe 'java'
 include_recipe 'nexus_iq_server::download'
 include_recipe 'nexus_iq_server::configure'
