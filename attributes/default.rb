ae0693fcad240af53b7e1b143919f160c4ff0963
Sonatype <sonatype-ci@sonatype.com>
2018-10-31 20:13:50 +0000

    Update IQ Server to 1.54.0-01

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@ default['java']['jdk_version'] = '8'
 default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['nexus_iq_server']['version'] = '1.53.0-01'
-default['nexus_iq_server']['checksum'] = 'd99c96285393cf6edc1d0034671861a5c40ad7dd8b9e688d0c6506a2fd696317'
+default['nexus_iq_server']['version'] = '1.54.0-01'
+default['nexus_iq_server']['checksum'] = 'd1037be6e77417651434b7e0757be87d8371e7191fee7d8e033be2cf5ae6b746'
 default['nexus_iq_server']['install_dir'] = '/opt/sonatype/nexus-iq-server'
 default['nexus_iq_server']['logs_dir'] = '/var/log/nexus-iq-server'
 default['nexus_iq_server']['conf_dir'] = '/etc/nexus-iq-server'
